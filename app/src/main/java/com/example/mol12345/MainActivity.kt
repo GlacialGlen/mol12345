91a360ed1131f30bd6357045be86a3719fd3756e
GlacialGlen <sunghyeck1779@gmail.com>
2022-06-30 14:13:40 +0900

    Finally Tabs

--- app/src/main/java/com/example/mol12345/MainActivity.kt
+++ app/src/main/java/com/example/mol12345/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.mol12345
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1) ViewPager2 참조
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+
+        // 2) FragmentStateAdapter 생성 : Fragment 여러개를 ViewPager2에 연결해주는 역할
+        val viewpagerFragmentAdapter = ViewpagerFragmentAdapter(this)
+
+        // 3) ViewPager2의 adapter에 설정
+        viewPager.adapter = viewpagerFragmentAdapter
+
+        // ViewPager2, TabLayout 참조
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+
+        // ###### TabLayout과 ViewPager2를 연결
+        // 1. 탭메뉴의 이름을 리스트로 생성해둔다.
+        val tabTitles = listOf<String>("Contact", "Gallery", "Calculator")
+
+        // 2. TabLayout과 ViewPager2를 연결하고, TabItem의 메뉴명을 설정한다.
+        TabLayoutMediator(tabLayout, viewPager, {tab, position -> tab.text = tabTitles[position]}).attach()
+
+    }
+}
+
+class ViewpagerFragmentAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+
+    // 1. ViewPager2에 연결할 Fragment 들을 생성
+    val fragmentList = listOf<Fragment>(Fragment01(), Fragment02(), Fragment03())
+
+    // 2. ViesPager2에서 노출시킬 Fragment 의 갯수 설정
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    // 3. ViewPager2의 각 페이지에서 노출할 Fragment 설정
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
     }
 }
